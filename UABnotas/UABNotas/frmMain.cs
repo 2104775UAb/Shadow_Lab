ed0bc68d1fb769a626618c7ae508a6a691caa18a
2101513UAB <168237505+2101513UAB@users.noreply.github.com>
2024-05-04 23:36:04 +0100

    Add files via upload

---
+++ UABnotas/UABNotas/frmMain.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace UABNotas {
+    public partial class frmMain : Form {
+        private View view;
+
+        public frmMain() {
+            InitializeComponent();
+        }
+
+        public View View { get => view; set => view = value; }
+
+        private void botaoNovaUC_Click(object sender, EventArgs e) {
+            view.ClicouNovaUC(sender, e);
+        }
+
+        internal void ConstruirFormas(ref List<UnidadesCurriculares> listaUCs) {
+
+            // Exemplo de teste
+            textBox1.Clear();  
+            foreach (UnidadesCurriculares uc in listaUCs)
+            {
+                textBox1.AppendText(uc.ToString() + Environment.NewLine);
+            }
+                
+        }
+
+        internal void ConstruirAnoSemestre(ref List<ListagemSemestreAno> listaUCs)
+        {
+
+            // Exemplo de teste
+            textBox2.Clear();
+            foreach (ListagemSemestreAno uc in listaUCs)
+            {
+                textBox2.AppendText(uc.ToString() + Environment.NewLine);
+            }
+
+        }
+
+
+    }
+}
